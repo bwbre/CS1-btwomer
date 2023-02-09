15443683b452fd5e9931266724d0cee1ef280f7f
bwbre <btwomer@mavs.coloradomesa.edu>
2023-02-08 20:53:24 -0700

    initial commit hw1 main.cpp

---
+++ assignments/stdio/main.cpp
@@ -0,0 +1,24 @@
+/*name: Brendan Womer
+This is my HW1 std-io assignment. today we will be drawing the visual stages of the gallows for a hang man game.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int firstName;
+    int lastName;
+
+    cout << "hello welcome to the early stages of my hangman game.\n Please enter your full name: ";
+    cin >> firstName >> lastName >> endl;
+
+    cout << "Hello " << firstName << lastName << endl;
+
+
+
+
+
+
+
+}
