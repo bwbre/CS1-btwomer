79d462cbab2cf86c7a6856c42edea0c0215155cd
bwbre <btwomer@mavs.coloradomesa.edu>
2023-02-15 11:34:09 -0700

    commit lecture notes

--- lecture/functions/functions.cpp
+++ lecture/functions/functions.cpp
@@ -5,29 +5,145 @@ Just like a mathmatical function, for each input(s) there is a unique output.
 setw() is a black box that takes in an int and modifies
 */
 
+/* pass by value is main() s1=42 at its memaddressX and prompt() s1=42 memaddressY.. prompt(s1) loses value unless returned.
+pass by reference is main() s1=42 with memaddressX is used in prompt() by reference, so s1 in prompt() uses same memadress
 
+*/
 
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
-void sayHello() {
-    cout << "hello from sayhello() new text " << endl;
+
+
+/*messing with bad over complicated use templates and types*/
+// int addNums(int num1, int num2) {
+//     return num1 + num2
+// }
+
+// float addNums(float num1, float num2) {
+//     return num1 + num2 + 10;
+// }
+
+// int addNums(float num1, int num2) {
+//     return num1 + num2;
+// }
+
+/* create template to recognize any data type (float, int) by using types*/
+// template <class type1, class type2>
+
+
+// type1 addnums(type1 num1, type2 num2) {
+//     type2 answer;
+//     answer = num1 + num2;
+//     return answer;
+
+// }
+
+
+/* function "prototype" before the actual function is defined allow it avoid issues of 
+fcns being called before they are created.. create empty fcn at beginning that MUST match
+the later defined function*/
+
+void sayhello();
+void saybye();
+
+template <class t1, class t2>
+t1 addnums(t1, t2);
+
+void sayhello() {
+    cout << "hello world" << endl;
+    saybye();
 }
 
-void getline(int, int){}
+void saybye() {
+    cout << "goodbye world" << endl;
+    sayhello();
+}
 
-float addNums(int num1, int num2) {
-    cout << "\n----------beginning of addnum()\n";
-    float result;
-    result = num1 + num2;   
-    cout << num1 << " <#3 addnum() #4> " << num2 << "\nthis is fcn float addNums() output of result is: " << result;
-    cout << "\n----------end of addnums()\n" << endl;
-    return result;
+//this is for organizing the reference passing example
+void random(){
+    
 
+
+void prompt(int&);
+
+
+
+int main() {
+    // sayhello();
+
+    int n1, n2; 
+
+    cout << "mem address for n1 and n2 " << &n1 << " " << &n2 << endl;
+  
+    prompt(n1);
+        cout << "mem address for n1 and n2 after prompt(n1) in main " << &n1 << " " << &n2 << endl;
+    prompt(n2);
+        cout << "mem address for n1 and n2 after prompt(n2) in main " << &n1 << " " << &n2 << endl;
+
+    n1 = 42.5;
+    n2 = 15.2;
+
+    cout << n1 << " + " << n2 << " = " << addnums<float, int>(n1, n2) << endl;
+    cout << n1 << " + " << n2 << " = " << addnums<int, float>(n1, n2) << endl;
+
+
+    return 0;
+}
+
+void prompt(int &n1) {
+    cout << "mem address for n1 inside promp() " << &n1 << endl;
 
 }
+
+template <class t1, class t2>
+t1 addnums(t1 num1, t2 num2) {
+    return num1 + num2;
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void sayHello() {
+//     cout << "hello from sayhello() new text " << endl;
+// }
+
+// void getline(int, int){}
+
+// float addNums(int num1, int num2) {
+//     cout << "\n----------beginning of addnum()\n";
+//     float result;
+//     result = num1 + num2;   
+//     cout << num1 << " <#3 addnum() #4> " << num2 << "\nthis is fcn float addNums() output of result is: " << result;
+//     cout << "\n----------end of addnums()\n" << endl;
+//     return result;
+
+
+// }
 
 // int main() {
 //     cout << "\n ---beginning of main()" << endl;
@@ -108,9 +224,3 @@ float addNums(int num1, int num2) {
 // }
 
 
-
-int main() {
-    
-
-
-}
