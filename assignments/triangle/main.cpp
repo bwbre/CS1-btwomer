90612155499e664e198a37cfedbebb1d80bd83e8
bwbre <btwomer@mavs.coloradomesa.edu>
2023-02-28 09:23:37 -0700

    monday morning commit

---
+++ assignments/triangle/main.cpp
@@ -0,0 +1,44 @@
+/*
+created by: Brendan Womer
+
+HW 3: Area and Perimeter of a Triangle
+not a moment after 2/27/2023
+
+notes: Will prompti user for three sides, then will calculate and print the output of
+the triangles area and perim
+*/
+
+//any include's and namespaces used
+
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+
+using namespace.std;
+
+//template fcns
+void calcarea();
+void calcperim();
+int promptside();
+
+
+//main fcn
+void main() {
+    int side1, side2, side3;
+    int sidetotal;
+
+    promptside(side1, side2, side3);
+
+
+
+
+}
+
+void promptside(double side1, double side2, double side3) {
+    cout << "Please input 3 sides of a given triangle: ";
+    getline(cin, side1, side2, side3);
+    cout << endl << "you inputted: \n" << "Side 1: " << side1 << endl << "Side 2: " << side2 << endl << "side 3: " << side3 << endl;
+
+
+
+}
--- lecture/conditionals/conditionals.cpp
+++ lecture/conditionals/conditionals.cpp
@@ -9,28 +9,95 @@ using namespace std;
 
 void sayhello();
 
+int main() 
+{
+    long num1;
+
+    cout <<"enter #"<<endl;
+    cin >> num1;
+
+    // if  (num1 < 0) 
+//     {
+//         num1 = abs(num1);
+//     }
+//
+//     if (num1%2 == 0 ) {
+//         cout << "is even" << endl;
+//         } 
+//     else {
+//         cout << "num1 is odd" << endl;
+//         }
+
+    // this is playing with switch fcn, checking if a number is even or odd
+    // switch(num1%2) { // switch is used to compare switch(beingcompared) with each case. each case is a value being tested. case '#' -- tests '#' against switch(beingcompared)
+    //     case 0;
+    //         if(num1 == 0) 
+    //         {
+    //             cout << "num1 is zero" << endl;
+    //         }
+    //         else 
+    //         {
+    //             cout << "num1 is even" << endl;
+    //         }
+    //         break; //use breaks for each class, doesnt matter if default has one. if    
+    //     case 1;
+    //         cout << "num1 is odd" << endl;
+    //         break; 
+    //     default;
+    //         cout << "how did u get here?";
+    //     }
+
+    // this is playing with if else fcns, checking if num1 is even or odd
+    // cout << "num1 is zero" << endl;
+    // if (num1 == 0 ) 
+    // {
+    //     cout << "num1 is zero" << endl;
+    // } 
+    // else if (num1&2 == 1 ) 
+    // {
+    //     cout << "num1 is odd " << endl;
+    // } 
+    // else if (num1%2 == 0) 
+    // {
+    //     cout << "num1 is even" << endl;
+    // } 
+    // else 
+    // {
+    //     cout << "the end, shouldnt get";
+    // }
+ }
+
+
+
+
+
 //initial use of if statement main()
-int main() {
-    int num1, num2;
-    int answer;
+// int main() {
+//     int num1, num2;
+//     int answer;
 
-    cout << "Please enter 2 numbers: " << endl;
-    cin >> num1 >> num2;
+//     cout << "Please enter 2 numbers: ";
+//     cin >> num1 >> num2;
 
-    answer = (num1 < num2);
+//     answer = (num1 < num2);
+//     cout << "is 1 less than 2? " << endl;
     
-    if (answer) {
-        //cout << "the number is not XXX";
-        cout << "num1 is less than to num2 " << endl;
-        sayhello();
+//     if (answer) {
+//         //cout << "the number is not XXX";
+//         cout << "num1 is less than to num2 \n" << endl;
+//         sayhello();
 
-    }
+//     } else {
+//         cout << "num1 is more than num2 \n" << endl;
 
-    cout << "outside and after if statement" << endl;
+//     }
 
-    return 0;
 
-}
+//     cout << "outside and after if statement" << endl;
+
+//     return 0;
+
+// }
 
 void sayhello() {
     cout << "hello from sayhello() " << endl;
