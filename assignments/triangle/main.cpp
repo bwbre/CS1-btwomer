9b6547d6f1657ef3204986ae8d40a69e0f962ddb
bwbre <btwomer@mavs.coloradomesa.edu>
2023-02-28 09:26:19 -0700

    trying to fix the name being in red, idk what that means.

--- assignments/triangle/main.cpp
+++ assignments/triangle/main.cpp
@@ -9,12 +9,11 @@ the triangles area and perim
 */
 
 //any include's and namespaces used
-
 #include<iostream>
 #include<cmath>
 #include<iomanip>
 
-using namespace.std;
+using namespace std;
 
 //template fcns
 void calcarea();
@@ -30,7 +29,7 @@ void main() {
     promptside(side1, side2, side3);
 
 
-
+    return 0;
 
 }
 
@@ -38,7 +37,4 @@ void promptside(double side1, double side2, double side3) {
     cout << "Please input 3 sides of a given triangle: ";
     getline(cin, side1, side2, side3);
     cout << endl << "you inputted: \n" << "Side 1: " << side1 << endl << "Side 2: " << side2 << endl << "side 3: " << side3 << endl;
-
-
-
 }
