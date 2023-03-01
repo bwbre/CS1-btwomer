f3a3385bd4d3a1435b752a4234ac6182960ac7e7
bwbre <btwomer@mavs.coloradomesa.edu>
2023-02-28 21:04:25 -0700

    definitely not submitting assignment

--- assignments/triangle/main.cpp
+++ assignments/triangle/main.cpp
@@ -16,25 +16,55 @@ the triangles area and perim
 using namespace std;
 
 //template fcns
-void calcarea();
-void calcperim();
-int promptside();
+float calcarea(float, float, float, float);
+float calcperim(float, float, float);
+float promptside(float, float, float);
 
 
 //main fcn
-void main() {
-    int side1, side2, side3;
-    int sidetotal;
+int main() {
+    float side1, side2, side3, area, perim;
 
-    promptside(side1, side2, side3);
+    cout << "please enter three sides of a triangle: ";
+
+    cin >> side1 >> side2 >> side3;
+
+    cout << "this is cout before calc perim in main()-- side1: " << side1 << " side2: " << side2 << " side3: " << side3 << endl;
+
+    cout << "in main(), result of calcperim() is: " << calcperim(side1, side2, side3) << endl;
+    perim = calcperim(side1, side2, side3);
+    cout << "in main(), result of calcarea() is: " << calcarea(side1, side2, side3, perim) << endl;
 
 
+    // calcarea(side1, side2, side3, perim);
     return 0;
 
 }
 
-void promptside(double side1, double side2, double side3) {
-    cout << "Please input 3 sides of a given triangle: ";
-    getline(cin, side1, side2, side3);
-    cout << endl << "you inputted: \n" << "Side 1: " << side1 << endl << "Side 2: " << side2 << endl << "side 3: " << side3 << endl;
+float calcperim(float s1, float s2, float s3) {
+    // cout << "\ninside of calc perim" << endl; //works well
+
+    //assigning float perim to the formula of triangle perim, with the sides that were given in main()
+    float perim = (.5*(s1+s2+s3));
+    // cout << "in calcperim -- calculating.... perim = " << perim << endl; // Works well
+
+    return perim;
 }
+
+float calcarea(float s1, float s2, float s3, float p) {
+    cout << "\ninside calcarea" << endl;
+
+    float area = sqrt(p*(p-s1)*(p-s2)*(p-s3));
+
+    cout << s1 << s2 << s3 << "area of a triangle with the given sides: ";
+
+    return area;
+}
+
+// float promptside(float side1, float side2, float side3) {
+//     cout << "Please input 3 sides of a given triangle: ";
+//     getline(cin, side1, side2, side3);
+//     cout << endl << "you inputted: \n" << "Side 1: " << side1 << endl << "Side 2: " << side2 << endl << "side 3: " << side3 << endl;
+
+//     return side1, side2, side3;
+// }
