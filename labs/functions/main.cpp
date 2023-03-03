11155c771148ebe6790f46a05915b6d5f3ae0a38
bwbre <btwomer@mavs.coloradomesa.edu>
2023-03-03 09:45:15 -0700

    i think its completely done, good to go.

--- labs/functions/main.cpp
+++ labs/functions/main.cpp
@@ -21,6 +21,10 @@ Algorithm steps:
 #include <cstdio>
 #include <cassert>
 #include <cmath>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
 using namespace std;
 
 const float epsilon = 1e-5; // 0.00001 accuracy upto 5 decimal points; error of margin
@@ -29,10 +33,11 @@ const float epsilon = 1e-5; // 0.00001 accuracy upto 5 decimal points; error of 
 
 // Function that calculates the distance between two points
 // x1, y1 and x2, y2 and returns the calculated value
-double findDistance(int, int, int, int);
+double findDistance(float, float, float, float);
 
 // test function that runs automated testing
 void test();
+
 
 
 // function clears the screen system call
@@ -50,85 +55,88 @@ int main()
 {
     int x1, y1, x2, y2; // variables to store two points (x1, y1) and (x2, y2)
     char ch;
-    char promptend = 'n';
+    // using pointers for funsies.
+    string *promptend;
 
-    //FIXME-bonus - 10 bonus points - add loop until user wants to quit
+    //FIXME-bonus - 10 bonus points - add loop until user wants to quit                              ----*fixed-----
     // the loop will execute the following block of code
 
     clearScreen();
     cout << "Program calculates distance between 2 points on a 2D coordinate." << endl;
 
-    while ((*promptend) != 'y'; (*promptend) != 'Y') 
-        {
-        cout << "Enter a point in the form (x, y): ";
+    while (*promptend != string("n") && *promptend != string("N")) 
+    {
+
+        cout << "\nEnter a point in the form (x, y): ";
         // parse the input stream
         cin >> ch >> x1 >> ch >> y1 >> ch; // value stored in ch is ignored
         printf("(x1, y1) = (%d, %d)\n", x1, y1);
 
 
-        //FIXME3 - Read/parse the second point and store data into variables x2 and y2
+        //FIXME3 - Read/parse the second point and store data into variables x2 and y2              ---*fixed*---
         cout << "Enter a second point in the form (x, y): ";
         cin >> ch >> x2 >> ch >> y2 >> ch;
-        printf("(x2, y2) = (%d, %d)", x1, x2);
+        printf("(x2, y2) = (%d, %d)\n", x1, x2);
 
 
-        //FIXME4 - Call test function
+        //FIXME4 - Call test function                                                               ----*fixed*-----
         test();
 
-
-        //FIXME5 - call findDistance function passing proper arguments
+        //FIXME5 - call findDistance function passing proper arguments                                ----*FIXED*----
         double distance;
         distance = findDistance(x1, y1, x2, y2);
 
 
-        //FIXME6 – Using printf function display the returned distance with proper description 
-        printf(distance);
+        //FIXME6 – Using printf function display the returned distance with proper description        ----*FIXED*----
+        
+        printf("The distance between the two sets of points is: %.2lf \n", distance);
 
-        cout << "Would you like to calculate another set of points? enter Y/N";
-        cin >> *promptend;
-        continue;
-        } 
-    if else () { 
-        break;
-        }
+        //prompt if user wants to loop
+        cout << "Would you like to calculate another set of points? enter 'Y' for yes: ";
 
+        string prompt;
+        cin >> prompt;
+        *promptend = prompt;
+
+    } 
 
 
     cin.ignore(1000, '\n');
-    cout << "Enter to quit the program: ";
+    cout << "Press enter to quit the program: ";
     cin.get();
     cout << "Good bye..." << endl;
     return 0;
 }
 
-double findDistance(int x1, int y1, int x2, int y2)
+double findDistance(float x1, float y1, float x2, float y2)
 {
-    // FIXME7 - Find the distance between (x1, y1) and (x2, y2)             ------*FIXED*------
-    double distance = (sqrt(((y2-y1)*(y2-y1))+sqrt(((x2-x1)*(x2-x1)))));        
+    // FIXME7 - Find the distance between (x1, y1) and (x2, y2)                                     ------*FIXED*------
+    double distance = (sqrt(((y2-y1)*(y2-y1))+(((x2-x1)*(x2-x1)))));        
     return distance;
 }
 
 // test function that test findDistance function with 3 test cases
 void test() 
 {
-    //test case 1
     float result; 
-    result = findDistance(4, 3, 5, 1);
     float expected; 
-    expected = 2.236067f;
+
+    //test case 1
+    result = findDistance(4, 3, 5, 1);
+    expected = 2.236067f; 
     assert( fabs(result - expected) <= epsilon); //accept the result if it's less than the error of margin
 
     //test case 2
     result = findDistance(3, 4, 5, 6);
-    expected = 2.828427f;
+    expected = 2.8284271247462;
     assert( fabs(result - expected) <= epsilon); //accept the result if it's less than the error of margin
 
     //test case 3
     result = findDistance(7, 8, 10, 11);
-    expected = 4.242640f;
+    expected = 4.2426406871193;
     assert( fabs(result - expected) <= epsilon); //accept the result if it's less than the error of margin
 
-    // FIXME8 - add at least two more test cases             -------*FIXED*-------
+    // FIXME8 - add at least two more test cases                                            -------*FIXED*-------
 
     cerr << "all tests passed..." << endl;
 }
--- lecture/loops/main.cpp
+++ lecture/loops/main.cpp
@@ -12,49 +12,49 @@ using namespace std;
 
 /* TESTING for() LOOPS, good for when you know how many time needs to run
 */
-int commentoutforloops () {
-    int i = 0;
-    int j = 0;
+// int commentoutforloops () {
+//     int i = 0;
+//     int j = 0;
     
-    /*  ---initialize for loop everything inside for (blah, blah, blah) is self contained inside the for ()....
-    --- for(declaration ; condition ; expression) as long as condition is true, will continue to run.
-        for(condition ; expression)
-    ---VARIABLES don't leave for loop, BUT values will. -> for (int i); wont leave, but for (i) will. 
-    ---if you cout << i; outside of for loop it wont work. usually use single characters like i or j.. unless prev line
-    ---"short" is an integer that is 16 bit instead of 32 bit. 
-    ---each iteration will wipe out values and run from beginning. if j = 0; inside for(), then each run it will reset back to 0,
-        need to define j = 0 in main()
-    ---if for( ; ;) then it will loop infinitely, no parameters of any kind, will run over and over.
-    ---if for( ; ;i++) will loops i forever
+//     /*  ---initialize for loop everything inside for (blah, blah, blah) is self contained inside the for ()....
+//     --- for(declaration ; condition ; expression) as long as condition is true, will continue to run.
+//         for(condition ; expression)
+//     ---VARIABLES don't leave for loop, BUT values will. -> for (int i); wont leave, but for (i) will. 
+//     ---if you cout << i; outside of for loop it wont work. usually use single characters like i or j.. unless prev line
+//     ---"short" is an integer that is 16 bit instead of 32 bit. 
+//     ---each iteration will wipe out values and run from beginning. if j = 0; inside for(), then each run it will reset back to 0,
+//         need to define j = 0 in main()
+//     ---if for( ; ;) then it will loop infinitely, no parameters of any kind, will run over and over.
+//     ---if for( ; ;i++) will loops i forever
 
 
-----listed increments are == to each other ------- 
-    //notes
-    i++ 
-    == 
-    //note
-    (i = i+1) 
-    == 
-    //
-    (i += 1) 
-    == 
-    //
-    (++i)
-    */
-    /*for (int i = 0, int j = 5; (i < 10) && (j > 0); i++ , j--) {
-        i = 0;
-        j = (j+1);
-    */
-    //will run i loop 10 times, each time will run nested loop 10 times.
-    for (int i = 0, int j = 5; (i < 10) && (j > 0); i++ , j--) {
-        for (int j = 0; (j < 10); j++) {
-            cout << " i: " << i << "j: " << j << endl
-            }
-        }
-        //will run loop and add hello world to EACH iteration
-        cout << i << ": hello world" << endl;
-        cout << "j: " << j << endl;
-}
+// ----listed increments are == to each other ------- 
+//     //notes
+//     i++ 
+//     == 
+//     //note
+//     (i = i+1) 
+//     == 
+//     //
+//     (i += 1) 
+//     == 
+//     //
+//     (++i)
+//     */
+//     /*for (int i = 0, int j = 5; (i < 10) && (j > 0); i++ , j--) {
+//         i = 0;
+//         j = (j+1);
+//     */
+//     //will run i loop 10 times, each time will run nested loop 10 times.
+//     for (int i = 0, int j = 5; (i < 10) && (j > 0); i++ , j--) {
+//         for (int j = 0; (j < 10); j++) {
+//             cout << " i: " << i << "j: " << j << endl
+//             }
+//         }
+//         //will run loop and add hello world to EACH iteration
+//         cout << i << ": hello world" << endl;
+//         cout << "j: " << j << endl;
+// }
 
 /*While loops are good when you dont know how many times you want loop to run
     while will run minimum 1 time
