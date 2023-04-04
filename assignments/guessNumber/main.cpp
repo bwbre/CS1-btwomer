4f4dcc11bb84b1fe8e177e6915d92cd65d8b658c
bwbre <btwomer@mavs.coloradomesa.edu>
2023-04-04 09:43:36 -0600

    commit

--- assignments/guessNumber/main.cpp
+++ assignments/guessNumber/main.cpp
@@ -26,6 +26,7 @@ Bonus: when user quits, program will provide stats of player
 #include <iostream>
 #include <cmath>
 #include <cassert>
+#include <random>
 
 using namespace std;
 
@@ -39,7 +40,7 @@ int main(int argc, char* argv[]) {
     //[0] # games played, [1] winrate, [2] lossrate
     int stats[3] = {0, 0, 0};
     string fullname;
-
+    srand(time(0));
     // cout << "Please enter your name: ";
     // getline(cin, fullname);
     // cout << "Greetings " << fullname << "!\n" << endl;
@@ -58,68 +59,89 @@ int game(int *stats) {
     //game ends after the 6th incorrect try.
     int guessnum = 0;
 
+
     //check will store the return value for checkGuess
     //0 means correct guess,
     //-1 means the guess < the random number
     //2 for guess > random number
     int check = 0;
 
+
+    //selection will hold the player's current guess.
+    int selection=0;
+
+
     //will add one to the number of games played.
-    cout << "\nstats[0]: " << stats[0] << endl;
+    // cout << "\nstats[0]: " << stats[0] << endl;
     stats[0] = (stats[0]+1);
-    cout << "\nstats[0]: " << stats[0] << endl;
+    // cout << "\nstats[0]: " << stats[0] << endl;
 
     //rnum will store the random number
     int rnum = randomNumber();
-    cout << "rnum: " << rnum << endl;
+    cout << "\nrnum: " << rnum << endl;
 
     //the game takes place in this loop.
     //will continue to loop until a correct guess.
     do {
+        cout << "\nGuess#: " << guessnum << "\n" << endl;
+        
+        if (guessnum ==7) {
+            cout << "Sorry, you're out of guesses. The number was: " << rnum << "\n" << endl;
+            break;
+        }
+
         guessnum++;
-        int selection = readNumber(stats);
-        cout << "readnum : " << selection << endl;
 
-        int check = checkGuess(rnum, selection);
-        cout << "Checkguess: " << check;
+        selection = readNumber(stats);
+        // cout << "\nselection : " << selection << endl;
+
+        check = checkGuess(rnum, selection);
+        // cout << "Checkguess: " << check << endl;
     }
+    while (check != 0);
 
-    while (check == 0);
+    cout << "game has ended." << endl;
 
     return 0;
 }  
 
 int randomNumber() {
-    int rnum = (rand()%20+1);
+    int rnum = random()%20+1;
+    cout << "rnum: " << rnum << endl;
+
     return rnum;
 }
 
 int readNumber(int *stats) {
     int selection = 0;
+
     //do loop will run over and over until the user selects a valid input.
     do {
         cout << "Please enter a number between 1 and 20: ";
-        cin >> selection;
-        // cin.clear();
+        cin >> selection; 
+        cout << endl;
 
-        //validate the player's input.
+        //validate the player's input is between 0-20.
         if (selection > 0 && selection < 21) {
-            cout << "\nDEBUG: valid\n";
+            // cout << "\nDEBUG: valid\n";
+            // cout << "DEBUG: SELECTION: " << selection << endl;
             return selection;
         }
         else {
             cout << "\nINVALID SELECTION.\n" << endl;
             selection = 0;
+            cin.clear();
         }
     }
     while (selection == 0);
 
-    return selection;
+    return 2020;
 }
 
 
 //returns: 0 if numbs are =, -1 if the first number is less than the second, 2 otherwise
 int checkGuess(int rnum, int selection) {
+
     //check selection against the random number.
     if (selection == rnum) { 
         cout << "Correct guess!\n" << endl;
@@ -129,11 +151,10 @@ int checkGuess(int rnum, int selection) {
         cout << "Incorrect. The number is smaller than your guess, " << selection << endl;
         return 2;
     }
-    else {
+    else if(selection < rnum) {
         cout << "Incorrect. The number is larger than your guess, " << selection << endl;
         return -1;
     }
-
 
 
     return 9000;
