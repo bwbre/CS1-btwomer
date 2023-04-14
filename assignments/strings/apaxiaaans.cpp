c8d79dcf45368f1b53320e411c71b7dd5706b967
bwbre <btwomer@mavs.coloradomesa.edu>
2023-04-14 15:55:26 -0600

    final submit? who knows.

--- assignments/strings/apaxiaaans.cpp
+++ assignments/strings/apaxiaaans.cpp
@@ -19,29 +19,33 @@ Find duplicate characters in a single input, no white space string ranging in si
 
 using namespace std;
 
-void searchname(string);
-int dupelength(string, int&, int&);
+string searchname(string);
+int dupelength(string, int&, int);
 string removedupe(string, int&, int&);
 void printname(string);
 int findlength(string);
+
+void printdebug(string, int&, int&);
 
 int main(int argc, char* argv[] ) {
     string name;
  
     cin >> name;
 
-    searchname(name);
+    name = searchname(name);
     // dupelength(name, cindex, dupelen);
     // removedupe(name, cindex, dupelen);
-    // printname(name);
+    printname(name);
 
 
 
     return 0;
 }
 
-void searchname(string name) {
-    //current index - this holds the value of index being searched. 
+string searchname(string name) {
+    /*current index - this holds the value of the current index being searched.
+     When a dupe is found this will act as a placeholder for where the program was scanning--
+     so it wont have to start scanning from the beginning */
     int cindex = 0;
 
     //the length of the series of duplicate letters found. local scope only
@@ -53,51 +57,58 @@ void searchname(string name) {
     for (int i = 0; i <= name.length(); i++) {
         //conditional looking for duplicate letters. by comparing the current index to the following index.
 
-        if (name.at(i+1) != string::npos && name[i+1] == name[i] ) {
+        if (name[i+1] != string::npos && name[i+1] == name[i] ) {
             //saves the current position of the index to be used as an argument.
-            cout << "found something" << endl;
+            // cout << "found something" << endl;
             cindex = i;
 
+            //find how many duplicate letters there are NOT COUNTING THE CURRENT INDEX. 
+            dupelen = dupelength(name, cindex, dupelen);
 
-            cout << "DEBUG: cindex = " << cindex << endl;
-            //find how many duplicate letters there are
-            dupelen = dupelength(name, cindex);
-            removedupe(name, cindex, dupelen);
+            // printdebug(name, cindex, dupelen);
+            if (dupelen > 0){
+                name = removedupe(name, cindex, dupelen);
+            }
         }
     }
+    // cout << "wtf " << endl;
+    return name;
 }
 
 //starting from the current index, will find the length of the series of duplicate letters.
-int dupelength(string name, int &cindex) {
-    //the length of the series of duplicate letters found.
-    int dupelen;
-
-    //position of the final duplicate letter found.
+int dupelength(string name, int &cindex, int dupelen) {
+    //position/index of the final duplicate letter found in char array.
     int dupeendi;
 
-    cout << "welcome to dupe length" << endl;
     //starting from the current index, continue iterating through the elements until a new character is found,
     //then declare the position of the final duplicate.
-    for (int i = cindex; i <= name.length(); i++) {
-        if (name.at(i+1) != string::npos && name[i] != name[i+1]){
+    for (int i = cindex; i < name.length(); i++) {
+        if (name[i] != name[i+1]){
+
+        //the index where the duplicate characters end.
         dupeendi = i;
-
-        cout << "DEBUG: dupeendi = " << dupeendi << "   i = " << i << endl; 
-        break;
+        //the difference between the current index and dupeend index is the dupe length.
+        dupelen = (dupeendi - cindex);
+        return dupelen;
         }
-
     }
 
-    //will find the difference between the current index and dupeend index AKA.. the dupe length.
-    dupelen = (dupeendi - cindex);
-    cout << "dupelen: " << dupelen << endl;
-
-    cin.clear();
-    cin.ignore(1000, '\n');
-
+    //return dupelen of 0 .... there are no more dupes
+    return 0;
 }
 
-void removedupe(string &name,int &cindex, int dupelen) {
-    
+string removedupe(string name,int &cindex, int &dupelen) {
+    //if statement to make sure that
+    // if (dupelen > 0) {
+        name.erase(cindex+1, dupelen);
+    // }
 
+    return name;
+}
+
+void printname(string name){
+    cout << name;
+}
+void printdebug(string name, int &cindex, int &dupelen) {
+    cout << "--DEBUG: cindex: " << cindex << "     dupelen: " << dupelen << "   name:" << name << endl;
 }
